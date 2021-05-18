b5973479416c4b6803e87f9638c0697df7445374
Giovanni Rosa <giovanni.rosa@unimol.it>
2021-05-18 19:15:47 +0200

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# CoRed
-Wrapper for code readability tool (Scalabrino et al. 2018 -  https://doi.org/10.1002/smr.1958)
+Extract both the files in a folder. The provided jar can compute the readability of a snippet and the readability of a class (mean readability of its methods). The output depends on the provided input file.
+
+To run the jar, use the following command, specifying at least a file name:
+java -jar rsm.jar {filename} [{filename2} {filename3} ... {filenameN}]
+
+Example:
+java -jar rsm.jar test.java
+
+You can also run it on many files. 
+Examples:
+java -jar rsm.jar test.java test2.java
+java -jar rsm.jar src/**/*.java
+
+You can export the values of all the metrics by using the following command:
+java -cp rsm.jar it.unimol.readability.metric.runnable.ExtractMetrics {filename}
+
+Example:
+java -cp rsm.jar it.unimol.readability.metric.runnable.ExtractMetrics test.java
---
+++ run.py
@@ -0,0 +1,50 @@
+import os, sys
+import subprocess
+import logging as log
+from types import resolve_bases
+
+
+def get_readability_level(readability_score):
+    if readability_score <= 0.4:
+        return "LOW"
+    elif readability_score > 0.4 and readability_score <= 0.6:
+        return "MEDIUM"
+    elif readability_score > 0.6:
+        return "HIGH"
+    else:
+        return ""
+
+
+# list java source files
+rootdir = sys.argv[1]
+
+java_files = list()
+for subdir, dirs, files in os.walk(rootdir):
+    for file in files:
+        if file.endswith('.java'):
+            java_files.append(os.path.join(subdir, file))
+
+# start CoRed
+process_out = list()
+p = subprocess.Popen(f'java -jar rsm.jar {" ".join(java_files)}', shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+for line in p.stdout.readlines():
+    process_out.append(line.decode('utf-8').strip())
+status = p.wait()
+
+output = list()
+if status == 0:
+    for line in process_out:
+        try:
+            row = line.split('\t')
+            row.append(get_readability_level(float(row[-1])))
+            output.append(row)
+            log.info(row)
+        except:
+            log.error("CoRed parsing error")
+else:
+    log.error(process_out)
+
+with open("report.csv", 'w') as out:
+    out.write("file_name,score,level\n")
+    for row in output:
+        out.write(",".join(row) + "\n")
